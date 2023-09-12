b5c48d638dfc79332c8ed742db00ba4b79feaa5d
Delaney Sander <dts487@usask.ca>
2023-09-12 11:45:44 -0600

    Added notes quick function to rofi quick menu

--- desktop/quick_menu.sh
+++ desktop/quick_menu.sh
@@ -9,10 +9,14 @@ if [ "$1" = "Email/Calendar" ]; then
   coproc ( obsidian > /dev/null 2>&1 )
   exit 0  
 elif [ "$1" = "Tuxworld" ]; then
-  coproc tux ( gnome-terminal -- sh -c "bash -c \"ssh tux; exec bash\"" )
+  coproc  ( gnome-terminal -- sh -c "bash -c \"ssh tux; exec bash\"" )
   exit 0
+elif [ "$1" = "Notes" ]; then
+  coproc ( obsidian > /dev/null 2>&1 )
+  coproc ( gnome-terminal -- sh -c "bash -c \"cd Vault; hx *; exec bash\"" )
 fi
 
 # Menu options
 echo "Email/Calendar"
 echo "Tuxworld"
+echo "Notes"
